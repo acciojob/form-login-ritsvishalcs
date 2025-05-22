cc5f376771fe697029ccc02d2715124fdd974609
Vishal Kumar <98923664+ritsvishalcs@users.noreply.github.com>
2025-05-22 12:19:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	const FirstName = document.querySelector('#fname').value
+	const LastName = document.querySelector('#lname').value
+	alert(FirstName + " " + LastName);
 }
